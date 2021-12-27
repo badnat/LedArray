2834b8e0e17f57a563f6e23a5f526d5290f38bc4
Nathaniel Badgett <badnat@live.unc.edu>
2021-12-27 14:03:35 -0500

    time to see the abstraction worked

--- LED/__main__.py
+++ LED/__main__.py
@@ -17,19 +17,19 @@ def main():
     if (param == "help"):
         print("possible params include : " + str(list(models.keys())))
 
-    # m: Model = models[sys.stdin](16)
-    # v: View = View(16, 0.05, board.D12)
-    # c: Controller = Controller(m, v)
-    # c.random(0.25)
+    m: Model = models[sys.stdin](16)
+    v: View = View(16, 0.05, board.D12)
+    c: Controller = Controller(m, v)
+    c.random(0.25)
 
-    # time.sleep(1)
-    # try:
-    #     while 1:
-    #         c.update()
-    #         time.sleep(0.2)
-    # except KeyboardInterrupt:
-    #     c.clear()
-    #     print("\n" + "has been interupted")
+    time.sleep(1)
+    try:
+        while 1:
+            c.update()
+            time.sleep(0.2)
+    except KeyboardInterrupt:
+        c.clear()
+        print("\n" + "has been interupted")
 
 if __name__ == "__main__":
     main()
