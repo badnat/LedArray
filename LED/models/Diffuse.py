c78426cb30c626c210db4a05107486c72f4e2ac4
Nathaniel Badgett <badnat@live.unc.edu>
2021-12-27 19:08:08 -0500

    deleted all the legacy code

---
+++ LED/models/Diffuse.py
@@ -0,0 +1,56 @@
+from Model import Model
+import numpy as np
+import sys
+# Model
+class Diffuse(Model):
+    def __init__(self, width: int):
+        Model.__init__(self, width)
+        self.pix2 = np.copy(self.pix2d)
+
+    def update(self) -> None:
+        self.pix2 = np.copy(self.pix2d)
+        for i in range(self.width):
+            for j in range(self.width):
+                self.pix2d[i, j] = self.diff(i, j)
+
+    def setup(self) -> None:
+        for i in range(self.width):
+            for j in range(self.width):
+                if(i <= 8):
+                    self.pix2d[i, j] = (255, 0, 0)
+                else:
+                    self.pix2d[i, j] = (0, 0, 255)
+    
+    def help(self) -> None:
+        print("This effect takes 2 params all probabilities from 0 to 100, <prob of led turning off> <prob of the led changing>")
+
+# update Logic
+    def diff(self, i, j):
+        if(i == 0 and j == 0):
+            n = -2 * self.pix2[i, j][1] + self.pix2[i+1, j][1] + self.pix2[i, j+1][1]
+            return (n, 0, 255 - n)
+        elif(i == 0 and j == 15):
+            n = -2 * self.pix2[i, j][1] + self.pix2[i+1, j][1] + self.pix2[i, j-1][1]
+            return (n, 0, 255 - n)
+        elif(i == 15 and j == 15):
+            n = -2 * self.pix2[i, j][1] + self.pix2[i-1, j][1] + self.pix2[i, j-1][1]
+            return (n, 0, 255 - n)
+        elif(i == 15 and j == 0):
+            n = -2 * self.pix2[i, j][1] + self.pix2[i-1, j][1] + self.pix2[i, j+1][1]
+            return (n, 0, 255 - n)
+        elif(i == 0):
+            n = -3 * self.pix2[i, j][1] + self.pix2[i+1, j][1] + self.pix2[i, j+1][1] + self.pix2[i, j-1][1]
+            return (n, 0, 255 - n)
+        elif(i == 15):
+            n = -3 * self.pix2[i, j][1] + self.pix2[i-1, j][1] + self.pix2[i, j+1][1] + self.pix2[i, j-1][1]
+            return (n, 0, 255 - n)
+        elif(j == 0):
+            n = -3 * self.pix2[i, j][1] + self.pix2[i, j+1][1] + self.pix2[i+1, j][1] + self.pix2[i-1, j][1]
+            return (n, 0, 255 - n)
+        elif(j == 15):
+            n = -3 * self.pix2[i, j][1] + self.pix2[i, j-1][1] + self.pix2[i+1, j][1] + self.pix2[i-1, j][1]
+            return (n, 0, 255 - n)
+        else:
+            n = -4 * self.pix2[i, j][1] + self.pix2[i+1, j][1] + self.pix2[i-1, j][1] + self.pix2[i, j+1][1] + self.pix2[i, j-1][1]
+            return (n, 0, 255 - n)
+
