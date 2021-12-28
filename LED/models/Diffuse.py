706ffcb35e3c62af3d058d61d7037585a44ba5ca
Nathaniel Badgett <badnat@live.unc.edu>
2021-12-27 19:35:52 -0500

    added Diffuse effect

--- LED/models/Diffuse.py
+++ LED/models/Diffuse.py
@@ -11,7 +11,7 @@ class Diffuse(Model):
         self.pix2 = np.copy(self.pix2d)
         for i in range(self.width):
             for j in range(self.width):
-                self.pix2d[i, j] = self.diff(i, j)
+                self.pix2d[i, j] = self.pix2d[i, j] - self.diff(i, j)
 
     def setup(self) -> None:
         for i in range(self.width):
@@ -27,39 +27,39 @@ class Diffuse(Model):
 # update Logic
     def diff(self, i, j):
         if(i == 0 and j == 0):
-            n = abs(-2 * self.pix2[i, j][0] + self.pix2[i+1, j][0] + self.pix2[i, j+1][0])
+            n = -2 * self.pix2[i, j][0] + self.pix2[i+1, j][0] + self.pix2[i, j+1][0]
             print(n, i, j)
             return (n, 0, 255 - n)
         elif(i == 0 and j == 15):
-            n = abs(-2 * self.pix2[i, j][0] + self.pix2[i+1, j][0] + self.pix2[i, j-1][0])
+            n = -2 * self.pix2[i, j][0] + self.pix2[i+1, j][0] + self.pix2[i, j-1][0]
             print(n, i, j)
             return (n, 0, 255 - n)
         elif(i == 15 and j == 15):
-            n = abs(-2 * self.pix2[i, j][0] + self.pix2[i-1, j][0] + self.pix2[i, j-1][0])
+            n = -2 * self.pix2[i, j][0] + self.pix2[i-1, j][0] + self.pix2[i, j-1][0]
             print(n, i, j)
             return (n, 0, 255 - n)
         elif(i == 15 and j == 0):
-            n = abs(-2 * self.pix2[i, j][0] + self.pix2[i-1, j][0] + self.pix2[i, j+1][0])
+            n = -2 * self.pix2[i, j][0] + self.pix2[i-1, j][0] + self.pix2[i, j+1][0]
             print(n, i, j)
             return (n, 0, 255 - n)
         elif(i == 0):
-            n = abs(-3 * self.pix2[i, j][0] + self.pix2[i+1, j][0] + self.pix2[i, j+1][0] + self.pix2[i, j-1][0])
+            n = -3 * self.pix2[i, j][0] + self.pix2[i+1, j][0] + self.pix2[i, j+1][0] + self.pix2[i, j-1][0]
             print(n, i, j)
             return (n, 0, 255 - n)
         elif(i == 15):
-            n = abs(-3 * self.pix2[i, j][0] + self.pix2[i-1, j][0] + self.pix2[i, j+1][0] + self.pix2[i, j-1][0])
+            n = -3 * self.pix2[i, j][0] + self.pix2[i-1, j][0] + self.pix2[i, j+1][0] + self.pix2[i, j-1][0]
             print(n, i, j)
             return (n, 0, 255 - n)
         elif(j == 0):
-            n = abs(-3 * self.pix2[i, j][0] + self.pix2[i, j+1][0] + self.pix2[i+1, j][0] + self.pix2[i-1, j][0])
+            n = -3 * self.pix2[i, j][0] + self.pix2[i, j+1][0] + self.pix2[i+1, j][0] + self.pix2[i-1, j][0]
             print(n, i, j)
             return (n, 0, 255 - n)
         elif(j == 15):
-            n = abs(-3 * self.pix2[i, j][0] + self.pix2[i, j-1][0] + self.pix2[i+1, j][0] + self.pix2[i-1, j][0])
+            n = -3 * self.pix2[i, j][0] + self.pix2[i, j-1][0] + self.pix2[i+1, j][0] + self.pix2[i-1, j][0]
             print(n, i, j)
             return (n, 0, 255 - n)
         else:
-            n = abs(-4 * self.pix2[i, j][0] + self.pix2[i+1, j][0] + self.pix2[i-1, j][0] + self.pix2[i, j+1][0] + self.pix2[i, j-1][0])
+            n = -4 * self.pix2[i, j][0] + self.pix2[i+1, j][0] + self.pix2[i-1, j][0] + self.pix2[i, j+1][0] + self.pix2[i, j-1][0]
             print(n, i, j)
             return (n, 0, 255 - n)
 
