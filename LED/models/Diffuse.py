bac7da3b339df8bec144d3e826d91a2003e0d05e
Nathaniel Badgett <badnat@live.unc.edu>
2021-12-27 19:48:05 -0500

    added Diffuse effect

--- LED/models/Diffuse.py
+++ LED/models/Diffuse.py
@@ -30,29 +30,29 @@ class Diffuse(Model):
     def diff(self, i, j):
         if(i == 0 and j == 0):
             n = (-2 * self.pix2[i, j][0] + self.pix2[i+1, j][0] + self.pix2[i, j+1][0])/255
-            return (n, 0, 255 - n)
+            return (n, 0, 1 - n)
         elif(i == 0 and j == 15):
             n = (-2 * self.pix2[i, j][0] + self.pix2[i+1, j][0] + self.pix2[i, j-1][0])/255
-            return (n, 0, 255 - n)
+            return (n, 0, 1 - n)
         elif(i == 15 and j == 15):
             n = (-2 * self.pix2[i, j][0] + self.pix2[i-1, j][0] + self.pix2[i, j-1][0])/255
-            return (n, 0, 255 - n)
+            return (n, 0, 1 - n)
         elif(i == 15 and j == 0):
             n = (-2 * self.pix2[i, j][0] + self.pix2[i-1, j][0] + self.pix2[i, j+1][0])/255
-            return (n, 0, 255 - n)
+            return (n, 0, 1 - n)
         elif(i == 0):
             n = (-3 * self.pix2[i, j][0] + self.pix2[i+1, j][0] + self.pix2[i, j+1][0] + self.pix2[i, j-1][0])/255
-            return (n, 0, 255 - n)
+            return (n, 0, 1 - n)
         elif(i == 15):
             n = (-3 * self.pix2[i, j][0] + self.pix2[i-1, j][0] + self.pix2[i, j+1][0] + self.pix2[i, j-1][0])/255
-            return (n, 0, 255 - n)
+            return (n, 0, 1 - n)
         elif(j == 0):
             n = (-3 * self.pix2[i, j][0] + self.pix2[i, j+1][0] + self.pix2[i+1, j][0] + self.pix2[i-1, j][0])/255
-            return (n, 0, 255 - n)
+            return (n, 0, 1 - n)
         elif(j == 15):
             n = (-3 * self.pix2[i, j][0] + self.pix2[i, j-1][0] + self.pix2[i+1, j][0] + self.pix2[i-1, j][0])/255
-            return (n, 0, 255 - n)
+            return (n, 0, 1 - n)
         else:
             n = (-4 * self.pix2[i, j][0] + self.pix2[i+1, j][0] + self.pix2[i-1, j][0] + self.pix2[i, j+1][0] + self.pix2[i, j-1][0])/255
-            return (n, 0, 255 - n)
+            return (n, 0, 1 - n)
 
